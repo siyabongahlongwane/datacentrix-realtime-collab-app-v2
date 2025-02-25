e61b610a574c812764069354e7777d53b169ccb0
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2025-02-26 00:04:51 +0200

    perf: Optimized document APIs using redis

--- backend/src/controllers/documentController.ts
+++ backend/src/controllers/documentController.ts
@@ -1,17 +1,39 @@
 import { Request, Response, NextFunction } from 'express';
 import { prisma } from '../../config/db';
 import asyncHandler from 'express-async-handler';
+import redisClient from '../../config/redis';
 
 export const getDocument = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const documentId = +req.params.id;
+    if (!documentId) {
+      const error = new Error('id is required');
+
+      Object.assign(error, { status: 400 });
+      throw error;
+    }
+    const cacheKey = `document:${documentId}`;
+
+    // Check if the document is in the cache
+    const cachedDocument = await redisClient.get(cacheKey);
+    if (cachedDocument) {
+      res.json(JSON.parse(cachedDocument));
+      return
+    }
+
+    // If not in cache, fetch from the database
     const document = await prisma.document.findUnique({
-      where: { id: +req.params.id },
+      where: { id: documentId },
     });
 
     if (!document) {
-      res.status(404).json({ error: 'Document not found' });
-      return;
+      const error = new Error('Document not found');
+      Object.assign(error, { status: 404 });
+      throw error;
     }
+
+    // Cache the document
+    await redisClient.set(cacheKey, JSON.stringify(document), { 'EX': 600 });
 
     res.json(document);
   } catch (error: any) {
@@ -23,18 +45,34 @@ export const getAllDocuments = asyncHandler(async (req: Request, res: Response, 
   try {
     const { id } = req.query;
     if (!id) {
-      res.status(400).json({ error: 'id is required' });
-      return;
+      const error = new Error('id is required');
+
+      Object.assign(error, { status: 400 });
+      throw error;
     }
 
+    const cacheKey = `documents:owner:${id}`;
+
+    // Check if the documents are in the cache
+    const cachedDocuments = await redisClient.get(cacheKey);
+    if (cachedDocuments) {
+      res.json(JSON.parse(cachedDocuments));
+      return
+    }
+
+    // If not in cache, fetch from the database
     const documents = await prisma.document.findMany({
       where: { owner_id: +id },
     });
 
     if (!documents.length) {
-      res.status(404).json({ error: 'No documents found' });
-      return;
+      const error = new Error('No documents found');
+      Object.assign(error, { status: 404 });
+      throw error;
     }
+
+    // Cache the documents
+    await redisClient.set(cacheKey, JSON.stringify(documents), { 'EX': 600 });
 
     res.json(documents);
   } catch (error: any) {
@@ -45,8 +83,9 @@ export const getAllDocuments = asyncHandler(async (req: Request, res: Response, 
 export const createDocument = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const { owner_id } = req.body;
   if (!owner_id) {
-    res.status(400).json({ error: 'id field is required' });
-    return;
+    const error = new Error('Owner id field is required');
+    Object.assign(error, { status: 400 });
+    throw error;
   }
 
   try {
@@ -61,8 +100,14 @@ export const createDocument = asyncHandler(async (req: Request, res: Response, n
     });
 
     if (!document?.id) {
-      throw new Error('Document creation failed');
+      const error = new Error('Document creation failed');
+      Object.assign(error, { status: 500 });
+      throw error;
     }
+
+    // Invalidate the cache for the owner's documents
+    const cacheKey = `documents:owner:${owner_id}`;
+    await redisClient.del(cacheKey);
 
     res.status(201).json({ success: true, msg: 'Document created successfully', id: document.id });
   } catch (error: any) {
