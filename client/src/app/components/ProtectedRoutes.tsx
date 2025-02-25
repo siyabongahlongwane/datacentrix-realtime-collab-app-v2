577cf64e959f3f80420e709e51075d34e7283067
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2025-02-25 15:39:57 +0200

    refactor: wait until user check has completed before assuming user is not logged in.

--- client/src/app/components/ProtectedRoutes.tsx
+++ client/src/app/components/ProtectedRoutes.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuthStore } from '@/app/store/useAuthStore';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "../store/useAuthStore"; // Adjust based on your setup
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-    const { isAuthenticated } = useAuthStore();
+    const { user } = useAuthStore();
     const router = useRouter();
+    const [checkingAuth, setCheckingAuth] = useState(true);
 
     useEffect(() => {
-        if (!isAuthenticated) {
-            router.push('/login');
+        if (!checkingAuth && !user) {
+            router.push("/login");
         }
-    }, [isAuthenticated, router]);
+        setCheckingAuth(false);
+    }, [user, router, checkingAuth]);
 
-    if (!isAuthenticated) {
-        return null;
+    if (checkingAuth || user === null) {
+        return <div className="flex items-center justify-center h-screen">Loading...</div>;
     }
 
     return <>{children}</>;
